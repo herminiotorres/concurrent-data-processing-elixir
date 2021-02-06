fc1226c2a6fe53363171ceaa12d63ea2408ba35a
Herminio Torres <herminiocesar@gmail.com>
2021-02-06 18:24:31 -0300

    Easy Concurrency With the Task Module

---
+++ sender/.iex.exs
@@ -0,0 +1,8 @@
+import_file_if_available("~/.iex.exs")
+
+emails = [
+  "hello@world.com",
+  "hola@world.com",
+  "nihao@world.com",
+  "konnichiwa@world.com"
+]
--- sender/lib/sender.ex
+++ sender/lib/sender.ex
@@ -1,18 +1,16 @@
 defmodule Sender do
-  @moduledoc """
-  Documentation for `Sender`.
-  """
+  #def send_email("konnichiwa@world.com" = email), do: raise "Oops, couldn't send email to #{email}!"
+  def send_email("konnichiwa@world.com" = _email), do: :error
 
-  @doc """
-  Hello world.
+  def send_email(email) do
+    Process.sleep(3_000)
+    IO.puts("Email to #{email} sent")
+    {:ok, "email_sent"}
+  end
 
-  ## Examples
-
-      iex> Sender.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def notify_all(emails) do
+    Sender.EmailTaskSupervisor
+    |> Task.Supervisor.async_stream_nolink(emails, &send_email/1)
+    |> Enum.to_list()
   end
 end
